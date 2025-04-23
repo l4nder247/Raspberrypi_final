c2bf36918b49069c7e9a1fc3aa5fab41358c40da
l4nder247 <163925495+l4nder247@users.noreply.github.com>
2025-04-23 11:01:33 -0400

    Update py_final.py

--- py_final.py
+++ py_final.py
@@ -1,27 +1,34 @@
 ###THIS IS THE CODE FOR THE HUMIDITY AND TEMP STATION
+#final_py_iteration2.py is the iteration with everything added 
 import BlynkLib
 from BlynkTimer import BlynkTimer
-from DHT11 import DHT11  # Make sure your file is renamed appropriately (e.g., dht_module.py)
+from DHT import dht11 # Make sure your file is renamed appropriately (e.g., dht_module.py)
 import time
 import adafruit_bmp280 # run this command "sudo pip3 install adafruit-circuitpython-bmp280 --break-system-packages"
 import board
+import smtplib
+from email.mime.text import MIMEText
+
 
 import csv
 import os
 
 
 # Initialize DHT11
-dht11 = DHT11(17)
+dht11 = dht11(23)
 # Intialize the thang
 i2c = board.I2C()
 bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c, 0x76)
 bmp280.sea_level_pressure = 1023.6  ########THIS LINE NEEDS TO BE CHANGED FOR THE DAILY AIR PRESSURE
 
 # you can do bmp280.temperature/pressure/altitude, and assign it to a varialbe where you can update it to blynk.
-print(bmp280.temperature)
-print(bmp280.pressure)
-print(bmp280.altitude)
+#print(bmp280.temperature)
+#print(bmp280.pressure)
+#print(bmp280.altitude)
 
+#create new csv file each time you run based on current date/time
+current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+CSV_FILE = f"weather_run_{current_time}.csv"
 
 
 # Blynk Auth Token
@@ -32,26 +39,67 @@ blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
 # Create BlynkTimer instance
 timer = BlynkTimer()
+original_dir = os.getcwd()
+data_folder = os.path.join(original_dir, 'datafiles')
 
 def hello_world():
     print("Start!")
 # blynk.virtual_write(5, 25)
 def publish_data():
+    email_timer = 6
     #blynk.virtual_write(5, 25)
-    result = dht11.get_result()
+    result = dht11.read_data()
     print("Raw result from DHT11:", result)
-    while True:
-        print(f"Temperature: {result[2]}")
-        blynk.virtual_write(5, result[2])
-        print(f"Humidity: {result[1]}")
-        blynk.virtual_write(4, result[1])
-        print(f"Pressure: {bmp280.pressure}")
-        blynk.virtual_write(1, bmp280.pressure)
+    while True:             
+        os.chdir(original_dir)
+        TEMP_THRESHOLD=10
+        HUMIDITY_THRESHOLD = 70
+        temp = result[1]
+        hum = result[0]
+        pressure = bmp280.pressure
 
+        alert_msg = ""
+        try:
+            if temp > TEMP_THRESHOLD:
+                alert_msg = f"High Temperature Alert: {temp}°C\n"
+
+            if hum > HUMIDITY_THRESHOLD:
+                alert_msg = f"High Humidity Alert: {hum}%\n"
+
+            if ((len(alert_msg)!=0) and (email_timer >10)):
+                msg = MIMEText(alert_msg)
+                msg['Subject'] = 'RPI Weather Alert'
+                msg['From'] = 'lucarasberrypi@gmail.com'
+                msg['To'] = 'landersb.tenzer@gmail.com'
+
+                with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+                    server.login('lucarasberrypi@gmail.com', 'xqpy cmus qlsn ehtb')
+                    server.send_message(msg)
+                    print("Alert email sent.")
+                    #reset the email timer
+                    
+                    email_timer = 0
+        except TypeError as e:
+            #sometimes, the temperature sensor likes to trip, and not work. This catches the error when trying to compare temp to temp_threshold, which throws a TypeError
+            print("Type Error ExceptionCaught: Hum and temp values empty")
+        
+        print()
+        print(f"Temperature: {temp}\n")
+        blynk.virtual_write(5, temp)
+        print(f"Humidity: {hum}\n")
+        blynk.virtual_write(4, hum)
+        print(f"Pressure: {pressure}\n\n")
+        blynk.virtual_write(1, pressure)
+        time.sleep(2)
+        email_timer = email_timer + 1
+        
+        
+        #Swap to the data directory
+        os.chdir(data_folder)
         with open(CSV_FILE, mode='a', newline='') as file:
             writer = csv.writer(file)
             timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-            writer.writerow([timestamp, tempVal, humVal])
+            writer.writerow([timestamp, temp, hum, pressure])
 
 # Add Timers
 timer.set_timeout(2, hello_world)
